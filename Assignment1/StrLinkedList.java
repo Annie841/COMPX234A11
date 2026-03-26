57bf2e2ef8f60d4d0291cf5916dadcb814559743
Annie841 <3951365369@qq.com>
2026-03-26 08:31:03 +0800

    Inner Node class

---
+++ Assignment1/StrLinkedList.java
@@ -0,0 +1,29 @@
+/**
+ * String Linked List Implementation for COMPX201 Assignment
+ * Stores and manages a list of String values using nodes
+ * Required for LottoDraw Task 2
+ */
+public class StrLinkedList {
+    private class Node {
+        String data;
+        Node next;
+        public Node(String data){
+            this .data = data ;
+            this.next = null;
+        }
+        
+
+ 
+
+
+    }
+    private Node Head;
+    public StrLinkedList(){
+        Head =null;
+    }
+    
+        
+    
+
+    
+}
